6ed4622b579f843527dfd039e03054526d150af7
toto <riedotoine@gmail.com>
2022-12-01 20:36:17 +0100

    add output

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -5,3 +5,20 @@ const configuration = new Configuration({
 });
 
 const openai = new OpenAIApi(configuration);
+const basePromptPrefix = "Write my a short bio for my twitter account please I don't want #. With those info: ";
+const generateAction = async(req, res)=>{
+
+    //run first prompt 
+    console.log(`API:${basePromptPrefix}${req.body.userInput}`)
+    const baseCompletation = await openai.createCompletion({
+        model:"text-davinci-003",
+        prompt: `${basePromptPrefix}${req.body.userInput}`,
+        temperature: 0.7,
+        max_tokens:250,
+    })
+
+    const basePromptOutput = baseCompletation.data.choices.pop();
+    res.status(200).json({output: basePromptOutput});
+};
+
+export default generateAction;
--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,30 @@ import { useState } from "react";
 
 const Home = () => {
   const [userInput, setUserInput] = useState("");
+  const [apiOutput, setApiOutput] = useState("");
+  const [isGenerating, setIsGenerating] = useState(false);
+0
+  const callGenerateEndpoint = async() => {
+  
+    setIsGenerating(true);
+    console.log("Calling openai...")
+
+    const response = await fetch('/api/generate', {
+      method: "POST",
+      headers:{
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({userInput})
+    });
+
+    const data = await response.json();
+    const {output} = data;
+    console.log("OpenAI  replied...", output.text)
+
+    setApiOutput(`${output.text}`);
+    setIsGenerating(false);
+  }
+
   const onUserChangedText = (event) => {
     setUserInput(event.target.value);
   };
@@ -25,19 +49,31 @@ const Home = () => {
         </div>
         <div className="prompt-container">
           <textarea
-            placeholder="start typing here"
+            placeholder="Write info you want to write in your bio"
             className="prompt-box"
             value={userInput}
             onChange={onUserChangedText}
           />
-          <div className="prompt-buttons">
-            <a className="generate-button" onClick={null}>
+        </div>
+        <div className="prompt-buttons">
+            <a className={isGenerating ? 'generate-button loading' : 'generate-button'} onClick={callGenerateEndpoint}>
               <div className="generate">
-                <p>Generate</p>
+              {isGenerating ? <span className="loader"></span> : <p>Generate</p>}
               </div>
             </a>
-          </div>
         </div>
+        {apiOutput &&(
+            <div className="output">
+              <div className="output-header-container">
+                <div className="output-header">
+                  <h3>Output</h3>
+                </div>
+              </div>
+              <div className="output-content">
+                <p>{apiOutput}</p>
+              </div>
+            </div>
+          )}
       </div>
       <div className="badge-container grow">
         <a
