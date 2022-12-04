186e30e399db5052a03d9fab25a7a9a1e347ee04
toto <riedotoine@gmail.com>
2022-12-04 17:54:03 +0100

    update path for card and title

--- pages/_document.js
+++ pages/_document.js
@@ -4,11 +4,11 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <meta property="og:title" content="Twitter Bio generator" key="title"/>
-        <meta property="og:description" content="GPT Twitter Bio Generator" key="description"/>
+        <meta property="og:title" content="Twitter Bio Generator" key="title"/>
+        <meta property="og:description" content="Twitter Bio Generator" key="description"/>
         <meta
           property="og:image"
-          content="https://ipfs.io/ipfs/QmNiAMzLp8sQMHgtfpnb35hvnExdcta4SYDkWdTtmJj7pc"
+          content="https://ipfs.io/ipfs/Qmcc7qV4Hr8YxYFPepH3a1iw8KZNfTbtDdHcBaCUL9aPHM/card-og.jpg"
         />
         <meta name="twitter:card" content="summary_large_image"></meta>
       </Head>
